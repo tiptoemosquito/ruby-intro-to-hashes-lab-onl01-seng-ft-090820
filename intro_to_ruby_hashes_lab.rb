96049cb8b5518a36a40582ec53592f7ac8274945
Christina Howard <clhoward33@icloud.com>
2020-09-14 18:50:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_second_tier
 	monopoly = {railrods: => {
     pieces: => 4,
     names: => {},rent_in_dollars: => {}
-    }} 
+    }}
     #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
   end
 end
